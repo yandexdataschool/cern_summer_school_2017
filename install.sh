efcf82d814a6259dd1561d649690e9d70ddff3f1
Nikita Kazeev <kazeevn@yandex-team.ru>
2017-07-31 19:08:32 +0200

    faster xgb install

--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-pip install tensorflow==1.2 keras==2.0.6 catboost==0.1.1.5 --user
-cd /tmp
-git clone --recursive https://github.com/dmlc/xgboost
-cd xgboost
-git checkout 0e06d1805d06fab063ee5f39563a6c6ad7510345
-make -j4
-cd python_package
-python setup.py install --user
+pip install tensorflow==1.2 keras==2.0.6 catboost==0.1.1.5 xgboost=0.6a2 --user
