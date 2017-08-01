14ed231b3abf5efa8f9d7f7f3dd032eea5da0d56
Nikita Kazeev <kazeevn@yandex-team.ru>
2017-08-01 11:42:16 +0200

    uer

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install tensorflow==1.2 keras==2.0.6 catboost==0.1.1.5 xgboost==0.6a2 --user
-pip install --upgrade scikit-learn==0.18.1
+pip install --upgrade scikit-learn==0.18.1 --user
