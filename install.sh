4230bb97b5a138fc45a8a64bb46c3c4c09088e83
Nikita Kazeev <kazeevn@yandex-team.ru>
2017-08-03 15:31:45 +0200

    Added installation

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt-get install -y graphviz
 pip install tensorflow==1.2 keras==2.0.6 catboost==0.1.1.5 xgboost==0.6a2 --user
 pip install --upgrade scikit-learn==0.18.1 --user
 pip install --upgrade h5py==2.7.0 --user
